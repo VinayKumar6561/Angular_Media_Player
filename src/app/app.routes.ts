4bdb5c4a05feba706f41100c2d28435276e2dfc9
VinayKumar6561 <vinayk6561@gmail.com>
2025-08-20 22:22:49 +0530

    Added side and navbar with some screens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home'; 
+import { About } from './components/about/about'; 
+import { VideoPlayerComponent } from './video-player/video-player'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'player', component: VideoPlayerComponent },
+  { path: '**', redirectTo: '' } 
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { VideoPlayerComponent } from './video-player/video-player'; 
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, VideoPlayerComponent],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, RouterModule, MatMenuModule, MatSidenavModule, MatListModule,MatIcon
+
+  ],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
-export class App {
-  // No code needed here
-}
+export class App {}
